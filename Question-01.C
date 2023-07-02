b7f8d57a8f4f694b649136e33cdb05609e68d7f1
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-02 20:07:59 +0530

    Update Question-01.C

--- Question-01.C
+++ Question-01.C
@@ -12,3 +12,21 @@ int main ()
     printf("Lenght is %d",i);
     return 0;
 }
+
+// ......................................................ANOTHER WAY ............................................................
+
+/*
+#include<stdio.h>
+
+int main ()
+{
+    char str[] = "Nilufa" ;
+
+    int i , count = 0 ; 
+    for(i=0 ; str[i] ; i++) 
+        count ++ ; 
+
+    printf("Lenght is %d",count);
+    return 0;
+}
+*/
